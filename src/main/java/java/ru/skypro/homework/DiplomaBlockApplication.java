aee2740b4f7d4e698552411fb4de704360790df8
InnaKolor1 <ineza.permiakova@yandex.ru>
2025-12-19 13:25:07 +0300

    1

---
+++ src/main/java/java/ru/skypro/homework/DiplomaBlockApplication.java
@@ -0,0 +1,13 @@
+package java.ru.skypro.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaBlockApplication.class, args);
+	}
+
+}
